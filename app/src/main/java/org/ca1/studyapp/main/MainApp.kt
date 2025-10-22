716909325b3f955a84674b879d3d0dd422cdb1fb
Ava <117838179+Ayyvahh@users.noreply.github.com>
2025-10-22 03:08:28 +0100

    Added interface for Tasks. Fixed TaskModel.

--- app/src/main/java/org/ca1/studyapp/main/MainApp.kt
+++ app/src/main/java/org/ca1/studyapp/main/MainApp.kt
@@ -1,18 +1,17 @@
 package org.ca1.studyapp.main
 
 import android.app.Application
-import org.ca1.studyapp.models.TaskModel
+import org.ca1.studyapp.models.TaskMemStore
 import timber.log.Timber
 
 
 class MainApp : Application() {
 
-    val tasks = ArrayList<TaskModel>()
+    val tasks = TaskMemStore()
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         Timber.i("Task started")
-//
     }
 }
--- app/src/main/java/org/ca1/studyapp/models/PlacemarkModel.kt
+++
@@ -1,5 +0,0 @@
-package org.ca1.studyapp.models
-
-data class TaskModel(var title: String = "", var description: String = "")
-
-
