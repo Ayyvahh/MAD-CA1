9166f06210693b49a7590d256f0abafee51fbf54
Ava <117838179+Ayyvahh@users.noreply.github.com>
2025-10-22 04:10:19 +0100

    Update/Edit tasks using ids added
    Signed-off-by: Ava <117838179+Ayyvahh@users.noreply.github.com>

--- app/src/main/java/org/ca1/studyapp/models/TaskModel.kt
+++ app/src/main/java/org/ca1/studyapp/models/TaskModel.kt
@@ -5,6 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class TaskModel(
+    var id: Long = 0,
     var title: String = "",
     var description: String = ""
 ) : Parcelable
--- app/src/main/java/org/ca1/studyapp/models/TaskStore.kt
+++ app/src/main/java/org/ca1/studyapp/models/TaskStore.kt
@@ -3,5 +3,6 @@ package org.ca1.studyapp.models
 interface TaskStore {
     fun findAll(): List<TaskModel>
     fun create(task: TaskModel)
+    fun update(task: TaskModel)
 }
 
